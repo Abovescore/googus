a81531e66adf35cc27ed2dc67fb7c01436d704c9
above <160554522+Abovescore@users.noreply.github.com>
2024-12-15 20:13:48 -0600

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,56 @@
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+local Mercury = loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))()
+
+local GUI = Mercury:Create{
+    Name = "Googus Hack",
+    Size = UDim2.fromOffset(600, 400),
+    Theme = Mercury.Themes.Rust,
+    Link = "https://github.com/Abovescore/Googus"
+}
+
+local MainTab = GUI:Tab{
+	Name = "Main",
+	Icon = "rbxassetid://74000494301350"
+}
+
+MainTab:Button{
+	Name = "Infinite Yield",
+	Description = nil,
+	Callback = function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+	end
+}
+
+-- game-specific tabs
+if game.PlaceId == 189707 or game.PlaceId == 7057417395 then -- natural disaster survival, cuando el
+		GUI:Notification{
+		Title = "Alert",
+		Text = "Game detected, NDS! New tab added.",
+		Duration = 3,
+		Callback = function() end
+	}
+	
+	local NDSTab = GUI:Tab{
+		Name = "NDS",
+		Icon = "rbxassetid://80353051055937"
+	}
+
+	NDSTab:Button{
+		Name = "Black Hole",
+		Description = nil,
+		Callback = function()
+			loadstring(game:HttpGet("https://raw.githubusercontent.com/Abovescore/Googus/Hack/scripts/blackhole.lua"))()
+		end
+	}
+	
+	NDSTab:Button{
+		Name = "Ring Parts",
+		Description = nil,
+		Callback = function()
+			loadstring(game:HttpGet("https://raw.githubusercontent.com/Abovescore/Googus/Hack/scripts/ringparts.lua"))()
+		end
+	}
+end
