ebe4bb570715b0eb008b7640fdbc63f8e95ee577
above <160554522+Abovescore@users.noreply.github.com>
2024-12-15 20:49:37 -0600

    free advertisement

--- scripts/advertisement.lua
+++ scripts/advertisement.lua
@@ -1,1 +1,15 @@
 repeat task.wait() until game:IsLoaded()
+
+local function SendChatMessage(message)
+    if TextChatService.ChatVersion == Enum.ChatVersion.TextChatService then
+        local textChannel = TextChatService.TextChannels.RBXGeneral
+        textChannel:SendAsync(message)
+    else
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
+    end
+end
+
+if game:GetService("Players").LocalPlayer.Name ~= "GoogusHack997" or game:GetService("Players").LocalPlayer.Name ~= "GoogusHack998" or game:GetService("Players").LocalPlayer.Name ~= "GoogusHack999" then
+  SendChatMessage("Gᴏᴏɢᴜꜱ Hᴀᴄᴋ™ Oɴ Tᴏᴘ")
+  SendChatMessage("ɢɪᴛʜᴜʙ/ᴀʙᴏᴠᴇꜱᴄᴏʀᴇ/ɢᴏᴏɢᴜꜱ")
+end
