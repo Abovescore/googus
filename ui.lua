6741262d1e98004469909640e173a711efb09d1f
above <160554522+Abovescore@users.noreply.github.com>
2024-12-15 21:26:52 -0600

    revert 'new tab'

--- ui.lua
+++ ui.lua
@@ -675,7 +675,7 @@ function Library:create(options)
 		AnchorPoint = Vector2.new(0, .5),
 		BackgroundTransparency = 1,
 		TextSize = 14,
-		Text = --[[options.Name]] "New Tab",
+		Text = options.Name,
 		Position = UDim2.new(0, 25, 0.5, 0),
 		TextXAlignment = Enum.TextXAlignment.Left,
 		Size = UDim2.new(1, -45, 0.5, 0),
